03152d75df137ee3607a864c39c355fda306dd17
hk5232 <kazamu132@gmail.com>
2022-04-02 20:11:00 +0900

    投稿画面作成

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,31 @@
+class QuizzesController < ApplicationController
+  before_action :basic_auth, only: [:new]
+  def index
+  end
+
+  def new
+    @quiz = Quiz.new
+  end
+
+  def create
+    @quiz = Quiz.new(quiz_params)
+    if @quiz.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+private
+  
+def quiz_params
+  params.require(:quiz).permit(:image, :card_name, :attack, :defense, :source_id)
+end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_FURIMA_USER'] && password == ENV['BASIC_FURIMA_PASSWORD']
+    end
+  end
+end
+
