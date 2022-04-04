e7eea6f60c91331d5264ca8f2fa38206dab54dae
hk5232 <kazamu132@gmail.com>
2022-04-04 11:35:18 +0900

    問題表示実装

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,7 @@
 class QuizzesController < ApplicationController
   before_action :basic_auth, only: [:new]
   def index
+    session[:source] = Quiz.where(source_id: :number)
   end
 
   def new
@@ -17,7 +18,15 @@ class QuizzesController < ApplicationController
   end
 
   def start
-    @quiz = Quiz.all
+    @test = params.permit!.to_hash
+    if params[:number] == "1" || params[:number] == "2" || params[:number] == "3"
+      @id = Quiz.where(source_id: @test["number"])
+    else
+      @id = Quiz.all
+    end
+    @random = @id.order("RAND()").limit(1)
+    @ans = Quiz.where(source_id: @test["number"])
+    @answer = @ans.order("RAND()").limit(3)
   end
 
 private
